73c0148c34433d0725d267535a22702e82247ded
Gil <g.f.hoben@gmail.com>
2017-01-23 19:12:37 +0000

    changed print for eventual Python3 compatibility

--- src/utils/downloads.py
+++ src/utils/downloads.py
@@ -39,14 +39,14 @@ def download(url, verbose, user_agent='wswp', num_retries=2):
 
     error = False
     if verbose:
-        print 'Downloading:', url
+        print('Downloading:', url)
     headers = {'User-agent': user_agent}
     request = urllib2.Request(url, headers=headers)
     try:
         html = urllib2.urlopen(request).read()
     except urllib2.URLError as e:
         if verbose:
-            print 'Download error:', e.reason
+            print('Download error:', e.reason)
         html = None
         if num_retries > 0:
             if hasattr(e, 'code') and 500 <= e.code < 600:
