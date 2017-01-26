d33ac2399a731f5d36c74cec7f17435feaa6d90e
Gil <g.f.hoben@gmail.com>
2017-01-26 08:15:58 +0000

    DataLoader gets home directory from Home class

--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -1,5 +1,6 @@
 import json
-import os
+
+from home import Home
 
 
 class DataLoader:
@@ -8,18 +9,18 @@ class DataLoader:
 
         self.numbering = numbering
         self.amino_acid_property = amino_acid_property
-        self.directory_name = os.path.dirname(os.path.abspath(__file__))
+        self.directory_name = Home().homedir
 
     def get_data(self):
         # type: () -> list (name of positions)
         if len(self.numbering) > 0:
-            with open('{}/NumberingSchemes/{}.txt'.format(self.directory_name, self.numbering), 'r') as f:
+            with open('{}/data/NumberingSchemes/{}.txt'.format(self.directory_name, self.numbering), 'r') as f:
                 data = [x.replace('\n', '') for x in f.readlines()]
         elif len(self.amino_acid_property) > 0:
             measurement = self.amino_acid_property[0]
             database = self.amino_acid_property[1]
 
-            with open('{}/AminoAcidProperties.json'.format(self.directory_name), 'r') as f:
+            with open('{}/data/AminoAcidProperties.json'.format(self.directory_name), 'r') as f:
                 data = json.load(f)[measurement][database]
         else:
             data = ''
