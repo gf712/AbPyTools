8e31e7e508dcc519271ac5d99ad4cf9f6b058e4d
Gil <g.f.hoben@gmail.com>
2017-01-27 13:59:03 +0000

    rewrote DataLoader to simplify code

--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -5,32 +5,70 @@ from home import Home
 # - best would be to put everything in json files with appropriate names
 # - or put everything in a single file -> could become confusing to edit
 
+
 class DataLoader:
 
-    def __init__(self, numbering='', amino_acid_property=[], misc=[]):
+    def __init__(self, data=[], data_type='', amino_acid_property=[]):
 
-        self.numbering = numbering
+        """
+
+        :param data: list with the information required to access data in json file
+        :param data_type: str must be one of the following: 'CDR_positions', 'NumberingSchemes' or 'AminoAcidProperties'
+        :param amino_acid_property: temporary parameter
+        :param misc: temporary parameter
+        """
         self.amino_acid_property = amino_acid_property
-        self.misc = misc
         self.directory_name = Home().homedir
+        self.data = data
+        self.data_types = ['CDR_positions', 'NumberingSchemes', 'AminoAcidProperties']
+
+        if data_type not in self.data_types:
+            raise ValueError("{} is not a valid data type. Available data types: {}".format(
+                data_type, ' ,'.join(self.data_types)))
+        else:
+            self.data_type = data_type
+
+        # checks values when object is instantiated
+        if self.data_type == 'CDR_positions' or self.data_type == 'NumberingSchemes':
+            if len(self.data) != 2:
+                raise ValueError("Expected 2, instead of {} values.".format(len(self.data)))
+            if self.data[0] not in ['chothia']:
+                raise ValueError("Got {}, but only {} is available".format(self.data[0], 'chothia'))
+            if self.data[1] not in ["light", "heavy"]:
+                raise ValueError("Got {}, but only light and heavy are available".format(self.data[1]))
+        else:
+            if len(self.data) != 2:
+                raise ValueError("Expected 2, instead of {} values.".format(len(self.data)))
+            if self.data[0] not in ["hydrophobicity", "pI", "MolecularWeight"]:
+                raise ValueError("Got {}, but only {} are available".format(self.data[0],
+                                                                            "hydrophobicity, pI and MolecularWeight"))
+
+            if self.data[1] not in ["kdHydrophobicity", "wwHydrophobicity", "hhHydrophobicity", "mfHydrophobicity",
+                                    "ewHydrophobicity", "EMBOSS", "DTASetect", "Solomon", "Sillero", "Rodwell",
+                                    "Wikipedia", "Lehninger", "Grimsley", "average", "monoisotopic"]:
+                raise ValueError("Got {}, but only {} are available".format(self.data[1],
+                                                                            """
+                                                                            kdHydrophobicity ,wwHydrophobicity,
+                                                                            hhHydrophobicity, mfHydrophobicity,
+                                                                            ewHydrophobicity, EMBOSS, DTASetect,
+                                                                            Solomon, Sillero, Rodwell,
+                                                                            Wikipedia, Lehninger, Grimsley,
+                                                                            average and monoisotopic
+                                                                            """
+                                                                            ))
 
     def get_data(self):
-        # type: () -> list (name of positions)
-        if len(self.numbering) > 0:
-            with open('{}/data/NumberingSchemes/{}.txt'.format(self.directory_name, self.numbering), 'r') as f:
-                data = [x.replace('\n', '') for x in f.readlines()]
 
-        elif len(self.misc) > 0:
-            with open('{}/data/{}'.format(self.directory_name, self.misc[0]), 'r') as f:
-                data = json.load(f)[self.misc[1]][self.misc[2]]
-
-        elif len(self.amino_acid_property) > 0:
-            measurement = self.amino_acid_property[0]
-            database = self.amino_acid_property[1]
-            with open('{}/data/AminoAcidProperties.json'.format(self.directory_name), 'r') as f:
-                data = json.load(f)[measurement][database]
-
+        if self.data_type == 'CDR_positions':
+            with open('{}/data/CDR_positions.json'.format(self.directory_name), 'r') as f:
+                # need to access numbering scheme and chain type
+                data = json.load(f)[self.data[0]][self.data[1]]
+        elif self.data_type == 'NumberingSchemes':
+            with open('{}/data/NumberingSchemes.json'.format(self.directory_name), 'r') as f:
+                # need to access numbering scheme and chain type
+                data = json.load(f)[self.data[0]][self.data[1]]
         else:
-            data = ''
+            with open('{}/data/AminoAcidProperties.json'.format(self.directory_name), 'r') as f:
+                data = json.load(f)[self.data[0]][self.data[1]]
 
         return data
