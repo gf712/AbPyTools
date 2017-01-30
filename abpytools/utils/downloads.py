692c44df30e25aac0f4ad2f6c16d875831dab326
Gil <g.f.hoben@gmail.com>
2017-01-30 19:06:36 +0000

    added timeout option to download

--- abpytools/utils/downloads.py
+++ abpytools/utils/downloads.py
@@ -2,19 +2,24 @@ from urllib import request, error
 
 
 class Download:
-    def __init__(self, url='', verbose=False):
+    def __init__(self, url='', verbose=False, timeout=5):
         self.url = url
         self.verbose = verbose
         self.html = ''
         self.error = False
+        self.timeout = timeout
 
     def download(self, user_agent='wswp', num_retries=2):
-        self.html, self.error = download(self.url, self.verbose,
-                                         user_agent=user_agent,
-                                         num_retries=num_retries)
+        # self.html, self.error = download(self.url, self.verbose, user_agent=user_agent, num_retries=num_retries,
+        #                                  timeout=self.timeout)
+        try:
+            self.html = download(self.url, self.verbose, user_agent=user_agent, num_retries=num_retries,
+                                 timeout=self.timeout)
+        except IOError:
+            raise ValueError("Could not download url.")
 
 
-def download(url, verbose, user_agent='wswp', num_retries=2, decoding_format='utf-8'):
+def download(url, verbose, user_agent='wswp', num_retries=2, decoding_format='utf-8', timeout=5):
     """
     Function to download contents from a given url
 
@@ -39,24 +44,25 @@ def download(url, verbose, user_agent='wswp', num_retries=2, decoding_format='ut
             string with contents of given url
     """
 
-    error = False
+    #html_error = False
     if verbose:
         print('Downloading:', url)
     headers = {'User-agent': user_agent}
     request_obj = request.Request(url, headers=headers)
     try:
-        with request.urlopen(request_obj) as response:
+        with request.urlopen(request_obj, timeout=timeout) as response:
             html = response.read()
     except error.URLError as e:
         if verbose:
             print('Download error:', e.reason)
-        html = None
-        if num_retries > 0:
-            if hasattr(e, 'code') and 500 <= e.code < 600:
-                # retry 5XX HTTP errors
-                return download(url, user_agent, num_retries - 1)[0]
-            # elif hasattr(e, 'code') and e.code == 404:
-            else:
-                error = True
+        # html = None
+        # if num_retries > 0:
+        #     if hasattr(e, 'code') and 500 <= e.code < 600:
+        #         # retry 5XX HTTP errors
+        #         return download(url, user_agent, num_retries - 1)[0]
+        #     # elif hasattr(e, 'code') and e.code == 404:
+        #     else:
+        #         html_error = True
+        raise IOError(e.reason)
 
-    return html.decode(decoding_format), error
+    return html.decode(decoding_format)
