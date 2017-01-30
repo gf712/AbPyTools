26b730726f21111175ad8905848081b31ace8289
Gil <g.f.hoben@gmail.com>
2017-01-30 19:03:22 +0000

    fixed setup.py to compile files and keep data directory

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
+# from setuptools import setup
 
 setup(
     name='AbPyTools',
     version='0.1',
-    packages=['abpytools', 'abpytools.utils', 'abpytools.antibody', 'abpytools.data'],
+    package_dir={'abpytools': 'abpytools'},
+    packages=['abpytools', 'abpytools.utils', 'abpytools.antibody', 'abpytools.analysis'],
+    package_data={'abpytools': ['data/*.json']},
     url='https://github.com/gf712/AbPyTools',
     license='MIT',
     author='Gil Ferreira Hoben',
     author_email='gil.hoben.16@ucl.ac.uk',
     description='Python package for antibody analysis',
-    install_requires=['numpy', 'tqdm', 'joblib']
+    # install_requires=['numpy', 'tqdm', 'joblib', 'seaborn', 'matplotlib']
 )
