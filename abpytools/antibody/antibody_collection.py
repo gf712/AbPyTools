4d79fac823e1eec1b8e17a538ed2a920c0c8d6b8
Gil <g.f.hoben@gmail.com>
2017-01-30 19:09:41 +0000

    simplified loading of antibody objects to one method

--- abpytools/antibody/antibody_collection.py
+++ abpytools/antibody/antibody_collection.py
@@ -15,13 +15,12 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 
 class AntibodyCollection:
-
     """
     Object containing Antibody objects and to perform analysis on the ensemble.
 
     Methods:
-        load_from_antibody_object: loads in Antibody objects provided in a list
-        load_from_fasta: parses a FASTA file and creates an Antibody object for each entry
+        load: parses a FASTA file and creates an Antibody object for each entry, or loads in Antibody objects provided
+        in a list
         name: returns a list with the names obtained from each Antibody object's name attribute
         sequence: returns a list with the sequences of all antibodies in this AntibodyCollection object
         hydrophobicity_matrix: returns a numpy array with the hydrophobicity of shape (number of ab, 158/138)
@@ -43,70 +42,45 @@ class AntibodyCollection:
 
         if antibody_objects is None:
             antibody_objects = []
+
         if not isinstance(antibody_objects, list):
             raise IOError("Expected a list, instead got object of type {}".format(type(antibody_objects)))
+
         if not all(isinstance(obj, Antibody) for obj in antibody_objects):
             raise IOError("Expected a list containing objects of type Antibody")
+
         if not isinstance(path, str):
             raise IOError("Expected a string containing the path to a FASTA format file")
+
         self._antibody_objects = antibody_objects
         self.chain = ''
         self._path = path
         self.n_ab = 0
 
-    def load_from_antibody_object(self, show_progressbar=True, n_jobs=-1):
+    def load(self, show_progressbar=True, n_jobs=-1):
 
-        print("Loading in antibody objects")
+        if self._path is not None:
 
-        # updated from stackoverflow answer in
-        # http://stackoverflow.com/questions/37804279/how-can-we-use-tqdm-in-a-parallel-execution-with-joblib
-        if show_progressbar:
-            aprun = parallelexecutor(use_bar='tqdm', n_jobs=n_jobs)
-        else:
-            aprun = parallelexecutor(use_bar='None', n_jobs=n_jobs)
-        self._antibody_objects = aprun(total=len(self._antibody_objects)) \
-            (delayed(load_antibody_object)(obj) for obj in self._antibody_objects)
+            with open(self._path, 'r') as f:
+                names = list()
+                sequences = list()
+                for line in f:
+                    if line.startswith(">"):
+                        names.append(line.replace("\n", "")[1:])
+                    else:
+                        sequences.append(line.replace("\n", ""))
+                if len(names) != len(sequences):
+                    raise IOError("Error reading file: make sure it is FASTA format")
 
-        chains = [x.chain for x in self._antibody_objects]
-        chains_without_na = [x for x in chains if x != 'NA']
+                self._antibody_objects = list()
 
-        skipped = len([x.chain for x in self._antibody_objects if x.chain == 'NA'])
+                for name, sequence in zip(names, sequences):
+                    self._antibody_objects.append(Antibody(name=name, sequence=sequence))
 
-        while 'NA' in chains:
-            i = chains.index('NA')
-            del self._antibody_objects[i], chains[i]
-
-        print("Skipped {} objects in list".format(skipped))
-
-        if len(set(chains_without_na)) == 1:
-            self.chain = chains_without_na[0]
-        else:
-            raise ValueError("All sequences must of the same chain type: Light or Heavy")
-
-        self.n_ab = len(chains_without_na)
-
-        if self.n_ab == 0:
-            raise IOError("Could not find any heavy or light chains in provided file or list of objects")
-
-    def load_from_fasta(self, show_progressbar=True):
-
-        with open(self._path, 'r') as f:
-            names = []
-            sequences = []
-            for line in f:
-                if line.startswith(">"):
-                    names.append(line.replace("\n", "")[1:])
-                else:
-                    sequences.append(line.replace("\n", ""))
-            if len(names) != len(sequences):
-                raise IOError("Error reading file: make sure it is FASTA format")
-
-        self._antibody_objects = list()
-
-        for name, sequence in zip(names, sequences):
-            self._antibody_objects.append(Antibody(name=name, sequence=sequence))
-
-        self.load_from_antibody_object(show_progressbar=show_progressbar)
+        self._antibody_objects, self.chain, self.n_ab = load_from_antibody_object(
+            antibody_objects=self._antibody_objects,
+            show_progressbar=show_progressbar,
+            n_jobs=n_jobs)
 
     def names(self):
         return [x.name for x in self._antibody_objects]
@@ -163,3 +137,39 @@ def parallelexecutor(use_bar='tqdm', **joblib_args):
         return tmp
 
     return aprun
+
+
+def load_from_antibody_object(antibody_objects, show_progressbar=True, n_jobs=-1):
+    print("Loading in antibody objects")
+
+    # updated from stackoverflow answer in
+    # http://stackoverflow.com/questions/37804279/how-can-we-use-tqdm-in-a-parallel-execution-with-joblib
+    if show_progressbar:
+        aprun = parallelexecutor(use_bar='tqdm', n_jobs=n_jobs)
+    else:
+        aprun = parallelexecutor(use_bar='None', n_jobs=n_jobs)
+    antibody_objects = aprun(total=len(antibody_objects)) \
+        (delayed(load_antibody_object)(obj) for obj in antibody_objects)
+
+    chains = [x.chain for x in antibody_objects]
+    chains_without_na = [x for x in chains if x != 'NA']
+
+    skipped = len([x.chain for x in antibody_objects if x.chain == 'NA'])
+
+    while 'NA' in chains:
+        i = chains.index('NA')
+        del antibody_objects[i], chains[i]
+
+    print("Skipped {} objects in list".format(skipped))
+
+    if len(set(chains_without_na)) == 1:
+        chain = chains_without_na[0]
+    else:
+        raise ValueError("All sequences must of the same chain type: Light or Heavy")
+
+    n_ab = len(chains_without_na)
+
+    if n_ab == 0:
+        raise IOError("Could not find any heavy or light chains in provided file or list of objects")
+
+    return antibody_objects, chain, n_ab
