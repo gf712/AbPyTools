85d0ea0186a8d8622585159824cfb0715b64b92c
Gil <g.f.hoben@gmail.com>
2017-02-02 13:29:51 +0000

    easier import of AminoAcidFreq class

--- abpytools/__init__.py
+++ abpytools/__init__.py
@@ -1,3 +1,4 @@
 from abpytools.antibody.antibody import Antibody
 from abpytools.antibody.antibody_collection import AntibodyCollection
 from abpytools.analysis.cdr_length import CDRLength
+from abpytools.analysis.amino_acid_freq import AminoAcidFreq
