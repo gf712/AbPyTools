20a5f8b5ad776ba63e474dec1002a89d9bd17b38
Gil <g.f.hoben@gmail.com>
2017-09-12 15:09:20 +0100

    minor test fix

--- tests/alignment_test.py
+++ tests/alignment_test.py
@@ -24,4 +24,4 @@ class AntibodyCollectionCore(unittest.TestCase):
     def test_needleman_wunsch_score_BLOSUM45(self):
         sa = SequenceAlignment(self.ab_collection_1[0], self.ab_collection_2, 'needleman_wunsch', 'BLOSUM45')
         sa.align_sequences()
-        self.assertEquals(sa.score[self.ab_collection_2.names[0]], 513)
+        self.assertEqual(sa.score[self.ab_collection_2.names[0]], 513)
