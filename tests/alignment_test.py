9266f95ab0653b6fd5abfbc826404c67c0105dee
Gil <g.f.hoben@gmail.com>
2017-09-13 15:19:01 +0100

    finished writing tests for SequenceAlignment

--- tests/alignment_test.py
+++ tests/alignment_test.py
@@ -2,7 +2,7 @@ import unittest
 from abpytools import ChainCollection, SequenceAlignment
 
 
-class AntibodyCollectionCore(unittest.TestCase):
+class SequenceAlignmentTests(unittest.TestCase):
 
     def setUp(self):
         self.ab_collection_1 = ChainCollection(path='./tests/sequence_alignment_seq_1.fasta')
@@ -37,8 +37,30 @@ class AntibodyCollectionCore(unittest.TestCase):
         sa.align_sequences()
         self.assertEqual(sa.aligned_sequences[self.ab_collection_2.names[0]], self.seq2_aligned)
 
+    def test_alignment_exception_1(self):
+        # catch exception when subsitution matrix is not known
+        self.assertRaises(ValueError, SequenceAlignment, self.ab_collection_1[0],
+                          self.ab_collection_2, 'needleman_wunsch', 'foo')
+
+    def test_alignment_exception_2(self):
+        # catch exception when algorithm is not known
+        sa = SequenceAlignment(self.ab_collection_1[0], self.ab_collection_2, 'foo', 'BLOSUM62')
+        self.assertRaises(ValueError, sa.align_sequences)
+
+    def test_alignment_indel_sign(self):
+        # if indel is positive the user receives a warning and indel will be set to -indel
+        sa = SequenceAlignment(self.ab_collection_1[0], self.ab_collection_2, 'needleman_wunsch', 'BLOSUM62')
+        sa.align_sequences(indel=-10)
+        sa_score_1 = sa.score
+        sa = SequenceAlignment(self.ab_collection_1[0], self.ab_collection_2, 'needleman_wunsch', 'BLOSUM62')
+        sa.align_sequences(indel=10)
+        sa_score_2 = sa.score
+        # since the second indel is positive it will be set to
+        # its negative, which runs the algorithms
+        # with the same params again and will get the same scores
+        self.assertEqual(sa_score_1, sa_score_2)
+
     def read_sequence_from_file(self, file):
         with open(file, 'r') as f:
             data = f.readlines()[0]
-
         return data
