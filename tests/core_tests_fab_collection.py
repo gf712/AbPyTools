dd1fcdbb35b1dd6903cab088576367f084184479
Gil <g.f.hoben@gmail.com>
2017-09-14 16:08:19 +0100

    FabCollection tests

---
+++ tests/core_tests_fab_collection.py
@@ -0,0 +1,96 @@
+import unittest
+from abpytools import FabCollection, ChainCollection
+from operator import itemgetter
+from urllib import request
+
+
+abnum_url = 'http://www.bioinf.org.uk/abs/abnum'
+
+
+# Helper functions
+def check_connection(URL, timeout=5):
+    try:
+        request.urlopen(url=URL, timeout=timeout)
+        return True
+    except request.URLError:
+        return False
+
+
+class FabCollectionCore(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.light_chain_collection = ChainCollection(path='./tests/chain_collection_light_2_sequences.json')
+        cls.heavy_chain_collection = ChainCollection(path='./tests/chain_collection_heavy_2_sequences.json')
+
+        cls.light_chain_collection.load(verbose=False, show_progressbar=False)
+        cls.heavy_chain_collection.load(verbose=False, show_progressbar=False)
+
+        cls.heavy_chain = cls.heavy_chain_collection[0]
+        cls.light_chain = cls.light_chain_collection[0]
+
+    def test_FabCollection_input_exception_1(self):
+        self.assertRaises(ValueError, FabCollection, None, 0, 0)
+
+    def test_FabCollection_input_exception_2(self):
+        self.assertRaises(ValueError, FabCollection, None, [0, 0], [0, 0])
+
+    def test_FabCollection_input_exception_3(self):
+        self.assertRaises(ValueError, FabCollection, None, self.heavy_chain, [0, 0])
+
+    def test_FabCollection_input_exception_4(self):
+        self.assertRaises(ValueError, FabCollection, None, [0, 0], self.light_chain)
+
+    def test_FabCollection_input_1(self):
+        self.assertIsInstance(FabCollection(light_chains=self.light_chain_collection,
+                                            heavy_chains=self.light_chain_collection),
+                              FabCollection)
+
+    def test_FabCollection_MW(self):
+        fab_collection = FabCollection(light_chains=self.light_chain_collection,
+                                       heavy_chains=self.heavy_chain_collection)
+        self.assertAlmostEqual(fab_collection.molecular_weight()[0], 25078.143331999992)
+
+    def test_FabCollection_ec_1(self):
+        fab_collection = FabCollection(light_chains=self.light_chain_collection,
+                                       heavy_chains=self.heavy_chain_collection)
+        self.assertEqual(fab_collection.extinction_coefficient()[0], 52870)
+
+    def test_FabCollection_ec_2(self):
+        fab_collection = FabCollection(light_chains=self.light_chain_collection,
+                                       heavy_chains=self.heavy_chain_collection)
+        self.assertAlmostEqual(fab_collection.extinction_coefficient(normalise=True)[0], 2.1082102969136987)
+
+    def test_FabCollection_charge(self):
+        fab_collection = FabCollection(light_chains=self.light_chain_collection,
+                                       heavy_chains=self.heavy_chain_collection)
+        self.assertAlmostEqual(fab_collection.charge().sum(), -2.0354649111988743)
+
+    def test_FabCollection_total_charge(self):
+        fab_collection = FabCollection(light_chains=self.light_chain_collection,
+                                       heavy_chains=self.heavy_chain_collection)
+        self.assertAlmostEqual(fab_collection.total_charge()[0], 3.3658691836339365)
+
+    @unittest.skipUnless(check_connection(URL=abnum_url), 'No internet connection, skipping test.')
+    def test_FabCollection_imgt_query_1(self):
+        fab_collection = FabCollection(light_chains=self.light_chain_collection,
+                                       heavy_chains=self.heavy_chain_collection)
+        fab_collection.imgt_server_query()
+        self.assertAlmostEqual(fab_collection.germline_identity['Heavy', 'Total'].iloc[0], 93.8)
+
+    @unittest.skipUnless(check_connection(URL=abnum_url), 'No internet connection, skipping test.')
+    def test_FabCollection_imgt_query_1(self):
+        # check germline assignment
+        fab_collection = FabCollection(light_chains=self.light_chain_collection,
+                                       heavy_chains=self.heavy_chain_collection)
+        fab_collection.imgt_server_query()
+        self.assertEqual(fab_collection.germline['Seq1 - LightSeq1']['Heavy'][0], 'IGHV1-8*01')
+
+    @unittest.skipUnless(check_connection(URL=abnum_url), 'No internet connection, skipping test.')
+    def test_FabCollection_imgt_query_1(self):
+        # check germline assignment score
+        fab_collection = FabCollection(light_chains=self.light_chain_collection,
+                                       heavy_chains=self.heavy_chain_collection)
+        fab_collection.imgt_server_query()
+        self.assertEqual(fab_collection.germline['Seq1 - LightSeq1']['Heavy'][1], 1.82e-66)
+
