5123c8dd9531701cdb8d2cb980f286ea8075f5b5
Gil <g.f.hoben@gmail.com>
2017-09-20 14:26:31 +0100

    added cache class

---
+++ abpytools/core/cache.py
@@ -0,0 +1,42 @@
+class Cache:
+    def __init__(self, max_cache_size=10):
+        self.max_cache_size = max_cache_size
+        self.cache = {}
+        self._cache_size = 0
+        self._cache_keys = []
+
+    def update(self, key, data, override=True):
+        # make space if the cache is full and override is True
+        if self._cache_size >= self.max_cache_size and override:
+            self._clear_cache()
+
+        if self._cache_size >= self.max_cache_size and not override:
+            raise ValueError("Cache is full, either increase the size of cache, "
+                             "remove items from cache or allow override")
+
+        if key not in self:
+            self.cache[key] = data
+            self._cache_keys.append(key)
+
+    def _clear_cache(self):
+        while self._cache_size >= self.max_cache_size:
+            # clear up cache until reaching max_cache_size - 1
+            self.remove_key(self._cache_keys[0])
+
+    def remove_key(self, key):
+        self.cache.pop(key)
+        self._cache_keys = self._cache_keys.pop(self._cache_keys.index(key))
+
+    def empty_cache(self):
+        self.cache = {}
+        self._cache_keys = []
+        self._cache_size = 0
+
+    def __getitem__(self, item):
+        return self.cache[item]
+
+    def __contains__(self, item):
+        if item in self._cache_keys:
+            return True
+        else:
+            return False
